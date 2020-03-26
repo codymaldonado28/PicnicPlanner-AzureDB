38fbe6a3bf647a90abec602b6c9de309de20d9bb
codymaldonado28 <codymaldonado28@yahoo.com>
2020-03-26 15:16:42 -0500

    Error Messages

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -11,20 +11,31 @@ export class HomeComponent {
   allAirports: any = null;
   SearchAirport: any;
   SearchedAirports: any;
-
+  Error: any;
   constructor(private _httpService: HttpService) { }
   ngOnInit() {
     this.SearchAirport='';
+    this.Error=null;
     this.getAllAirports();
   }
   getAllAirports() {
     if(this.SearchAirport.length>0){
+      this.Error=null;
       this.allAirports= null;
       console.log(this.SearchAirport)
       let obs = this._httpService.AirportSearch(this.SearchAirport)
       obs.subscribe(data => {
+        console.log(data)
+        this.allAirports=data;
+        if(this.allAirports.length == 0){
+          this.Error=`Could Not Find Airport ${this.SearchAirport}`;
+          this.SearchAirport=''
+          this.getAllAirports();
+        }
+        else{
         console.log("Searched Airports Loading")
         this.allAirports = data;
+        }
       })
       }
     else{
