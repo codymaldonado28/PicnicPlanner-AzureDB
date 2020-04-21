fd98f93585386fbab530c6fabdb67f1132cab272
codymaldonado28 <codymaldonado28@yahoo.com>
2020-04-21 11:09:24 -0500

    Unit Testing And Title fix

--- ClientApp/src/app/http.service.spec.ts
+++ ClientApp/src/app/http.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpService } from './http.service';
+import { HttpClientTestingModule, HttpTestingController  } from '@angular/common/http/testing';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http'
 
 describe('HttpService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [HttpService]
     });
   });
--- ClientApp/src/app/show-airport/show-airport.component.ts
+++ ClientApp/src/app/show-airport/show-airport.component.ts
@@ -24,37 +24,38 @@ export class ShowAirportComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this._route.params.subscribe(params => 
-    this.AirportId=params
+    this._route.params.subscribe(params =>
+      this.AirportId = params
     )
     console.log(this.AirportId)
     this.getAirport();
     this.getRunways();
   }
-  getAirport(){
+  getAirport() {
     let obs = this._httpService.showAirport(this.AirportId)
     obs.subscribe(data => {
       console.log(data);
-      if(data == null){
+      if (data == null) {
         console.log("No Data")
-        this.Error="No Airport Data"
+        this.Error = "No Airport Data"
       }
-      else{this.Airport=data;
-      this.GetWeather(this.Airport.latitude_deg, this.Airport.longitude_deg)
+      else {
+      this.Airport = data;
+        this.GetWeather(this.Airport.latitude_deg, this.Airport.longitude_deg)
       }
     })
   }
-  getRunways(){
-    this.RunwayData=false;
+  getRunways() {
+    this.RunwayData = false;
     let obs = this._httpService.getRunways(this.AirportId)
     obs.subscribe(data => {
       this.AirportRunways = data;
       console.log(this.AirportRunways);
-      if(this.AirportRunways.length>0){
+      if (this.AirportRunways.length > 0) {
         console.log("THere is some data");
-        for(var i =0; i<this.AirportRunways.length; i++){
-          if(this.AirportRunways[i].le_Heading_deg != 3 || this.AirportRunways[i].he_Heading_deg !=3){
-            this.RunwayData=true;
+        for (var i = 0; i < this.AirportRunways.length; i++) {
+          if (this.AirportRunways[i].le_Heading_deg != 3 || this.AirportRunways[i].he_Heading_deg != 3) {
+            this.RunwayData = true;
             break;
           }
         }
@@ -62,72 +63,99 @@ export class ShowAirportComponent implements OnInit {
       console.log(this.RunwayData)
     })
   }
-  GetWeather(lat,long){
+  GetWeather(lat, long) {
     let obs = this._httpService.getWeather(lat, long)
-      obs.subscribe(data => {
-        console.log(data)
-        this.Weather=data;
-      })
+    obs.subscribe(data => {
+      console.log(data)
+      this.Weather = data;
+    })
   }
-  ChooseDate(date){
-    this.PicnicDate=null;
-    this.PicnicDate=date;
+  ChooseDate(date) {
+    this.PicnicDate = null;
+    this.PicnicDate = date;
     console.log(this.PicnicDate);
     this.WindDir = this.PicnicDate.wind.deg
     console.log(this.WindDir)
-    this.GetFlightPath()
+    return this.GetFlightPath()
   }
-  UnChooseDate(date){
-    this.PicnicDate=null;
+  UnChooseDate(date) {
+    this.PicnicDate = null;
   }
-  DegToDirection(deg){
+  DegToDirection(deg) {
     var degree = deg % 360;
-    if (degree < 22.5){
+    if (degree >= 337.5 && degree < 22.5) {
       return "North"
     }
-    else if (degree >= 22.5 && degree < 67.5){
+    else if (degree >= 22.5 && degree < 67.5) {
       return "North East"
     }
-    else if(degree >= 67.5 && degree < 112.5){
+    else if (degree >= 67.5 && degree < 112.5) {
       return "East"
     }
-    else if (degree >= 112.5 && degree < 157.5 ){
+    else if (degree >= 112.5 && degree < 157.5) {
       return "South East"
     }
-    else if (degree >= 157.5 && degree < 202.5){
+    else if (degree >= 157.5 && degree < 202.5) {
       return "South"
     }
-    else if(degree >= 202.5 && degree < 247.5){
+    else if (degree >= 202.5 && degree < 247.5) {
       return "South West"
     }
-    else if (degree >= 247.5 && degree < 292.5){
+    else if (degree >= 247.5 && degree < 292.5) {
       return "West"
     }
-    else{
+    else {
       return "North West"
     }
-    }
-    GetFlightPath(){
-      if (this.RunwayData){
-        let degsArray: number[] = [];
-        for(var i =0; i<this.AirportRunways.length; i++){
-          if(this.AirportRunways[i].le_Heading_deg != 3){
-            degsArray.push(this.AirportRunways[i].le_Heading_deg)
-          }
-          if(this.AirportRunways[i].he_Heading_deg != 3){
-            degsArray.push(this.AirportRunways[i].le_Heading_deg)
-          }
-        }
-        let min= Math.abs(this.PicnicDate.wind.deg - degsArray[0]);
-        let minSpot=0;
-        for(var j=1; j<degsArray.length; j++){
-          if (degsArray[j] < min){
-            min = degsArray[j]
-            minSpot = j
-          }
-        }
-        this.FlightPath=degsArray[minSpot];
-        console.log(this.FlightPath);
-      }
-    }
   }
+  GetFlightPath() {
+    this.FlightPath = null;
+    if (this.RunwayData) {
+      let degsArray: number[] = [];
+      for (var i = 0; i < this.AirportRunways.length; i++) {
+        if (this.PicnicDate.wind.deg > 180) {
+          if (this.AirportRunways[i].le_Heading_deg != 3 && this.AirportRunways[i].le_Heading_deg < this.PicnicDate.wind.deg - 180) {
+            degsArray.push(this.AirportRunways[i].le_Heading_deg + 360)
+          }
+          else if (this.AirportRunways[i].le_Heading_deg != 3) {
+            degsArray.push(this.AirportRunways[i].le_Heading_deg)
+          }
+          if (this.AirportRunways[i].he_Heading_deg != 3 && this.AirportRunways[i].he_Heading_deg < this.PicnicDate.wind.deg - 180) {
+            degsArray.push(this.AirportRunways[i].he_Heading_deg + 360)
+          }
+          else if (this.AirportRunways[i].he_Heading_deg != 3) {
+            degsArray.push(this.AirportRunways[i].he_Heading_deg)
+          }
+        }
+        else {
+          if (this.AirportRunways[i].le_Heading_deg != 3 && this.AirportRunways[i].le_Heading_deg > this.PicnicDate.wind.deg + 180) {
+            degsArray.push(this.AirportRunways[i].le_Heading_deg - 360)
+          }
+          else if (this.AirportRunways[i].le_Heading_deg != 3) {
+            degsArray.push(this.AirportRunways[i].le_Heading_deg)
+          }
+          if (this.AirportRunways[i].he_Heading_deg != 3 && this.AirportRunways[i].he_Heading_deg > this.PicnicDate.wind.deg + 180) {
+            degsArray.push(this.AirportRunways[i].he_Heading_deg - 360)
+          }
+          else if (this.AirportRunways[i].he_Heading_deg != 3) {
+            degsArray.push(this.AirportRunways[i].he_Heading_deg)
+          }
+        }
+      }
+      console.log(degsArray);
+      let min = Math.abs(this.PicnicDate.wind.deg - degsArray[0]);
+      let minSpot = 0;
+      for (var j = 1; j < degsArray.length; j++) {
+        if (Math.abs(this.PicnicDate.wind.deg - degsArray[j]) < min) {
+          min = Math.abs(this.PicnicDate.wind.deg - degsArray[j])
+          minSpot = j
+        }
+        console.log(min)
+        console.log(minSpot)
+      }
+      this.FlightPath = degsArray[minSpot];
+      console.log(this.FlightPath);
+    }
+    return this.FlightPath
+  }
+}
