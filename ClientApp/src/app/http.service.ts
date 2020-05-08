988ebeb4ec26de6d26d0ca3655e8067928fb5188
codymaldonado28 <codymaldonado28@yahoo.com>
2020-05-07 23:09:07 -0500

    Change HttpService to  call azure api  http request

--- ClientApp/src/app/http.service.ts
+++ ClientApp/src/app/http.service.ts
@@ -8,23 +8,23 @@ export class HttpService {
 
   constructor(private _http: HttpClient) { }
   getAllAirports(){
-    return this._http.get('http://localhost:5000/api/airports');
+    return this._http.get('https://picnicplan2.azurewebsites.net/api/airports');
   }
   showAirport(airportId){
     console.log(airportId);
     var airportid = airportId.airportId;
     console.log(airportid);
-    return this._http.get(`http://localhost:5000/api/airport/${airportid}`)
+    return this._http.get(`https://picnicplan2.azurewebsites.net/api/airport/${airportid}`)
   }
   AirportSearch(search){
     console.log(search);
-    return this._http.get(`http://localhost:5000/api/airport/search/${search}`)
+    return this._http.get(`https://picnicplan2.azurewebsites.net/api/airport/search/${search}`)
   }
   getRunways(airportId){
     console.log(airportId);
     var airportid = airportId.airportId;
     console.log(airportid);
-    return this._http.get(`http://localhost:5000/api/runways/${airportid}`)
+    return this._http.get(`https://picnicplan2.azurewebsites.net/api/runways/${airportid}`)
   }
   getWeather(airportLat, airportLong){
     return this._http.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${airportLat}&lon=${airportLong}&units=imperial&appid=9e2b9f940e6b4d113e7c1872cf312d69`)
